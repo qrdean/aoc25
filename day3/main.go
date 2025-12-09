68c9f2fa3fe82c86a0c0d464899acb40cedefbbc
Quinton Dean <dean.quinton@ymail.com>
2025-12-09 10:52:30 -0600

    sovle day 3

--- day3/main.go
+++ day3/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 	"strconv"
 )
 
@@ -16,58 +18,132 @@ func getCurrentJoltage(val1, val2 int) int {
 	return number
 }
 
+func getCurrentJoltageArr(vals [12]BatteryNode) int {
+	concatenate := ""
+	for _, val := range vals {
+		concatenate += fmt.Sprintf("%d", val.Value)
+	}
+	number, _ := strconv.Atoi(concatenate)
+	return number
+}
+
 type BatteryNode struct {
 	Value    int
 	Position int
 }
 
 func getBatteryValue(bank []int) int {
-	first := BatteryNode{Value:0, Position: 0}
-	second := BatteryNode{Value:0, Position: 0}
-	inCase := 0
-	for i := len(bank) - 1; i >= 0; i-- {
-		if second.Value < bank[i] {
-			second.Value = bank[i]
-			second.Position = i
+	if len(bank) == 0 {
+		return 0
+	}
+	first := BatteryNode{Value: bank[len(bank)-2], Position: len(bank) - 2}
+	second := BatteryNode{Value: bank[len(bank)-1], Position: len(bank) - 1}
+	for i := len(bank) - 3; i >= 0; i-- {
+		if first.Value <= bank[i] {
+			temp := first.Value
+			tempPos := first.Position
+			first.Value = bank[i]
+			first.Position = i
+
+			if second.Value < temp {
+				second.Value = temp
+				second.Position = tempPos
+			}
 		}
-		if first.Value < second.Value {
-			temp := first
-			first = second
-			second = temp		
+	}
+	return getCurrentJoltage(first.Value, second.Value)
+}
+
+func getBatteryValuePt2(bank []int) int {
+	BatLimit := 12
+	var batteryNodes [12]BatteryNode
+	for i := range BatLimit {
+		offset := BatLimit - i
+		initPosition := len(bank) - offset
+		batteryNodes[i] = BatteryNode{Value: bank[initPosition], Position: initPosition}
+	}
+	fmt.Println(bank)
+	for i := len(bank) - (BatLimit + 1); i >= 0; i-- {
+		fmt.Println(batteryNodes)
+		for j := range batteryNodes {
+			if bank[i] >= batteryNodes[j].Value {
+				temp := batteryNodes[j].Value  
+				tempPos := batteryNodes[j].Position
+				if j-1 >= 0 {
+					if batteryNodes[j-1].Position >= i {
+						break
+					} 
+				}
+				batteryNodes[j].Value = bank[i]
+				batteryNodes[j].Position = i
+				shiftBattery(batteryNodes[:], temp, tempPos, j+1)
+				break
+			}
+		}
+	}
+	val := getCurrentJoltageArr(batteryNodes)
+	fmt.Println(val)
+	return val
+}
+
+func shiftBattery(batteryNodes []BatteryNode, next int, nextPos int, j int) {
+	// fmt.Println(batteryNodes[j])
+	temp := batteryNodes[j].Value  
+	tempPos := batteryNodes[j].Position
+	if batteryNodes[j].Value <= next {
+		batteryNodes[j].Value = next
+		batteryNodes[j].Position = nextPos
+		if j+1 < len(batteryNodes) {
+			shiftBattery(batteryNodes, temp, tempPos, j+1)
 		}
 	}
 }
 
-func getHighestTwo(bank []int) (BatteryNode, BatteryNode) {
-	currentJoltage := 0
-	highOne := BatteryNode{Value: 0, Position: 0}
-	highTwo := BatteryNode{Value: 0, Position: 0}
-	// bankNode := BankNode{First: &highOne, Second: &highTwo}
-	for i, x := range bank {
-		if highTwo.Value < x {
-			highTwo.Value = x
-			highTwo.Position = i
-		}
-		if (getCurrentJoltage(highOne.Value, highTwo.Value) > currentJoltage) && highOne.Value < highTwo.Value {
-			temp := highOne.Value
-			tempPos := highOne.Position
-			highOne.Value = highTwo.Value
-			highOne.Position = highTwo.Position
-			highTwo.Value = temp
-			highTwo.Position = tempPos
-		}
-
-		if getCurrentJoltage(highOne.Value, highTwo.Value) > currentJoltage {
-			currentJoltage = getCurrentJoltage(highOne.Value, highTwo.Value)
-		}
+func solve1(input [][]int) int {
+	total := 0
+	for _, bank := range input {
+		total += getBatteryValue(bank)
 	}
-	fmt.Println(currentJoltage)
-	return highOne, highTwo
+	return total
 }
 
-func solve1(input string) int {
-	return 0
+func solve2(input [][]int) int {
+	total := 0
+	for _, bank := range input {
+		total += getBatteryValuePt2(bank)
+	}
+	return total
+}
+
+func parseFile() [][]int {
+	file, err := os.Open("./part1.txt")
+	if err != nil {
+		panic(err)
+	}
+	scanner := bufio.NewScanner(file)
+	lines := []string{}
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	arrays := [][]int{}
+	for _, line := range lines {
+		arrays = append(arrays, parseLargeStringIntoArray(line))
+	}
+	return arrays
+}
+
+func parseLargeStringIntoArray(strLineInput string) []int {
+	array := make([]int, 0, len(strLineInput))
+
+	for _, char := range strLineInput {
+		digit, _ := strconv.Atoi(string(char))
+		array = append(array, digit)
+	}
+	return array
 }
 
 func main() {
+	lines := parseFile()
+	result := solve2(lines)
+	fmt.Println(result)
 }
--- day3/main_test.go
+++ day3/main_test.go
@@ -1,76 +1,101 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"strings"
+	"testing"
+)
 
 func TestMain(t *testing.T) {
-	// testInput := "987654321111111\n811111111111119\n234234234234278\n818181911112111\n"
-	// t.Run("test", func(t *testing.T) {
-	// 	got := solve1(testInput)
-	// 	want := 357
-	// 	if got != want {
-	// 		t.Errorf("got: %d, want:%d", got, want)
-	// 	}
-	// })
 
-	t.Run("get highest two nodes", func(t *testing.T) {
-		highest, secondHighest := getHighestTwo([]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1})
-		if highest.Value != 9 {
-			t.Errorf("highest value is not 9 but %d", highest.Position)
-		}
-		if highest.Position != 0 {
-			t.Errorf("highest pos is not 0 but %d", highest.Position)
-		}
+	t.Run("check battery values", func(t *testing.T) {
 
-		if secondHighest.Value != 8 {
-			t.Errorf("secondHighest value is not 9 but %d", secondHighest.Position)
+		batteryValue := getBatteryValue([]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1})
+		want := 98
+		if batteryValue != want {
+			t.Errorf("batteryValue: %d is not %d", batteryValue, want)
 		}
-		if secondHighest.Position != 1 {
-			t.Errorf("secondHighest pos is not 0 but %d", secondHighest.Position)
+		batteryValue= getBatteryValue([]int{8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9})
+		want = 89
+		if batteryValue != want {
+			t.Errorf("batteryValue: %d is not %d", batteryValue, want)
 		}
+		batteryValue= getBatteryValue([]int{2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8})
+		want = 78
+		if batteryValue != want {
+			t.Errorf("batteryValue: %d is not %d", batteryValue, want)
+		}
+		batteryValue= getBatteryValue([]int{8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1})
+		want = 92
+		if batteryValue != want {
+			t.Errorf("batteryValue: %d is not %d", batteryValue, want)
+		}
+	})
+	
+	t.Run("parse large number into array", func(t *testing.T) {
+		testInput := "123456789"
+		got := parseLargeStringIntoArray(testInput)
+		want := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+		if !reflect.DeepEqual(got, want) {
+    	t.Errorf("got: %v want :%v", got, want)
+		}
+		
+		testInput = "2555245573282137352766682525526364435746545343523394355638332326665366122245646523573255525564158774"
+		got = parseLargeStringIntoArray(testInput)
+		want = []int{2, 5, 5, 5, 2, 4, 5, 5, 7, 3, 2, 8, 2, 1, 3, 7, 3, 5, 2, 7, 6, 6, 6, 8, 2, 5, 2, 5, 5, 2, 6, 3, 6, 4, 4, 3, 5, 7, 4, 6, 5,4,5,3, 4,3,5,2,3,3,9,4,3,5,5,6,3,8,3,3,2,3,2,6,6,6,5,3,6,6,1,2,2,2,4,5,6,4,6,5,2,3,5,7,3,2,5,5,5,2,5,5,6,4,1,5,8,7,7,4}
+		if !reflect.DeepEqual(got, want) {
+    	t.Errorf("got: %v want :%v", got, want)
+		}
+		
+	})
 
-		highest, secondHighest = getHighestTwo([]int{8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9})
-		if highest.Value != 9 {
-			t.Errorf("highest value is not 9 but %d", highest.Position)
+	t.Run("some test", func(t *testing.T) {
+		testRealInput := "2555245573282137352766682525526364435746545343523394355638332326665366122245646523573255525564158774\n5413546422442523229295232262733493414148154532639862333271175557334235334296623642342223646213476455\n5433332355353372756453622653442176834335623343626343236463374522272945534432336513564562366234336223"
+		testThing := strings.Split(testRealInput, "\n")
+		results := []int{98, 99, 96}
+		for i, line := range testThing {
+			bank := parseLargeStringIntoArray(line)
+			val := getBatteryValue(bank)
+			if val != results[i] {
+				t.Errorf("got: %d, want :%d", val, results[i])
+			}
 		}
-		if highest.Position != 14 {
-			t.Errorf("highest pos is not 15 but %d", highest.Position)
-		}
+	})
+	
+	t.Run("check battery values pt2", func(t *testing.T) {
 
-		if secondHighest.Value != 8 {
-			t.Errorf("secondHighest value is not 8 but %d", secondHighest.Position)
+		batteryValue := getBatteryValuePt2([]int{9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1})
+		want := 987654321111
+		if batteryValue != want {
+			t.Errorf("batteryValue: %d is not %d", batteryValue, want)
 		}
-		if secondHighest.Position != 0 {
-			t.Errorf("secondHighest pos is not 0 but %d", secondHighest.Position)
+		batteryValue= getBatteryValuePt2([]int{8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9})
+		want = 811111111119
+		if batteryValue != want {
+			t.Errorf("batteryValue: %d is not %d", batteryValue, want)
 		}
+		batteryValue= getBatteryValuePt2([]int{2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8})
+		want = 434234234278
+		if batteryValue != want {
+			t.Errorf("batteryValue: %d is not %d", batteryValue, want)
+		}
+		batteryValue= getBatteryValuePt2([]int{8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1})
+		want = 888911112111
+		if batteryValue != want {
+			t.Errorf("batteryValue: %d is not %d", batteryValue, want)
+		}
+	})
 
-		highest, secondHighest = getHighestTwo([]int{2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8})
-		if highest.Value != 8 {
-			t.Errorf("highest value is not 9 but %d", highest.Position)
-		}
-		if highest.Position != 14 {
-			t.Errorf("highest pos is not 15 but %d", highest.Position)
-		}
-
-		if secondHighest.Value != 7 {
-			t.Errorf("secondHighest value is not 8 but %d", secondHighest.Position)
-		}
-		if secondHighest.Position != 13 {
-			t.Errorf("secondHighest pos is not 0 but %d", secondHighest.Position)
-		}
-
-		highest, secondHighest = getHighestTwo([]int{8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1})
-		if highest.Value != 9 {
-			t.Errorf("highest value is not 9 but %d", highest.Position)
-		}
-		if highest.Position != 6 {
-			t.Errorf("highest pos is not 6 but %d", highest.Position)
-		}
-
-		if secondHighest.Value != 1 {
-			t.Errorf("secondHighest value is not 8 but %d", secondHighest.Position)
-		}
-		if secondHighest.Position != 0 {
-			t.Errorf("secondHighest pos is not 0 but %d", secondHighest.Position)
+	t.Run("pt2 test real", func(t *testing.T) {
+		testRealInput := "2555245573282137352766682525526364435746545343523394355638332326665366122245646523573255525564158774\n5413546422442523229295232262733493414148154532639862333271175557334235334296623642342223646213476455\n5433332355353372756453622653442176834335623343626343236463374522272945534432336513564562366234336223"
+		testThing := strings.Split(testRealInput, "\n")
+		results := []int{987564158774, 999996676455, 966666436223}
+		for i, line := range testThing {
+			bank := parseLargeStringIntoArray(line)
+			val := getBatteryValuePt2(bank)
+			if val != results[i] {
+				t.Errorf("got: %d, want :%d", val, results[i])
+			}
 		}
 	})
 }
